a213dfe966ebf6c89b9e4b4bb9b0992a7a8dd471
Adokshaj Bhandarkar <badokshaj57@gmail.com>
2021-03-28 10:11:14 +0530

    changed admin page details (titles).

--- atmaa/atmaa/urls.py
+++ atmaa/atmaa/urls.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from authapp import views
 
+#added to change the admin page stuff
+admin.site.site_header = "Harry Ice Cream Admin"
+admin.site.site_title = "Harry Ice Cream Admin Portal"
+admin.site.index_title = "Welcome to Harry Ice Creams"
+#added to change the admin page stuff
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('authapp.urls'))
