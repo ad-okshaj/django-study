dd68230b1c0103f95b3170d312956029a0d17c43
Adokshaj Bhandarkar <badokshaj57@gmail.com>
2021-03-28 09:53:20 +0530

    fixed templateerror, admin page working + navbar dark

--- atmaa/authapp/views.py
+++ atmaa/authapp/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render, HttpResponse
 
 
 def index(request):
-   #context = {
-    #   "x" : "this is sent"
-    #}
-    return HttpResponse('this is home.') 
-   # return render(request, 'index.html', context)
+    context = {
+        "x" : "this is sent"
+    }
+   # return HttpResponse('this is home.') 
+    return render(request, 'index.html', context)
     
 def about(request):
     return HttpResponse('this is about page.')
